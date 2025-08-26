1849c484dc2735b9d1ebd7f004d3f3174d2bba6c
Hafsa Akmal <hafsaakmal003@gmail.com>
2025-08-26 20:00:30 +0500

    Update: db.json sequelize for production env

--- src/config/db.js
+++ src/config/db.js
@@ -2,8 +2,21 @@ const { Sequelize } = require('sequelize');
 const dotenv = require('dotenv');
 dotenv.config();
 
+console.log("🚨 DATABASE_URL at runtime:", process.env.DATABASE_URL);
+
+
 const sequelize = process.env.DATABASE_URL
-  ? new Sequelize(process.env.DATABASE_URL, )
+  ? new Sequelize(process.env.DATABASE_URL, {
+      dialect: 'postgres',
+      protocol: 'postgres',
+      dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false, // for self-signed certs; adjust as needed
+        },
+      },
+      logging: false,
+    })
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
       host: process.env.DB_HOST,
       port: process.env.DB_PORT,
